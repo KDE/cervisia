7a1d6b440b9cb00bfe3865e6fda7fb7cda39caa4
Christian Loose <christian.loose@hamburg.de>
2003-08-26 08:28:38 +0000

    * --warning * small whitespace cleanup * better variable names
    svn path=/trunk/kdesdk/cervisia/; revision=245008

--- logplainview.cpp
+++ logplainview.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  *  Copyright (c) 2003 Christian Loose <christian.loose@hamburg.de>
  *
  * This program may be distributed under the terms of the Q Public
@@ -22,6 +22,8 @@
 
 #include "loginfo.h"
 
+using namespace Cervisia;
+
 
 LogPlainView::LogPlainView(QWidget* parent, const char* name)
     : KTextBrowser(parent, name)
@@ -38,35 +40,35 @@ LogPlainView::~LogPlainView()
 }
 
 
-void LogPlainView::addRevision(const Cervisia::LogInfo& logInfo)
+void LogPlainView::addRevision(const LogInfo& logInfo)
 {
     setTextFormat(QStyleSheet::RichText);
 
     // assemble revision information lines
     QString logEntry;
-    
-    logEntry += "<b>" + i18n("revision %1").arg(QStyleSheet::escape(logInfo.m_revision)) + 
+
+    logEntry += "<b>" + i18n("revision %1").arg(QStyleSheet::escape(logInfo.m_revision)) +
                 "</b>";
-    logEntry += " &nbsp;[<a href=\"revA#" + QStyleSheet::escape(logInfo.m_revision) + "\">" + 
+    logEntry += " &nbsp;[<a href=\"revA#" + QStyleSheet::escape(logInfo.m_revision) + "\">" +
                 i18n("Select for revision A") +
                 "</a>]";
-    logEntry += " [<a href=\"revB#" + QStyleSheet::escape(logInfo.m_revision) + "\">" + 
+    logEntry += " [<a href=\"revB#" + QStyleSheet::escape(logInfo.m_revision) + "\">" +
                 i18n("Select for revision B") +
                 "</a>]<br>";
-    logEntry += "<i>" + 
+    logEntry += "<i>" +
                 i18n("date: %1; author: %2").arg(QStyleSheet::escape(logInfo.dateTimeToString()))
                                             .arg(QStyleSheet::escape(logInfo.m_author)) +
                 "</i>";
 
     append(logEntry);
-           
+
     setTextFormat(QStyleSheet::PlainText);
 
     const QChar newline('\n');
 
     // split comment in separate lines
     QStringList lines = QStringList::split(newline, logInfo.m_comment, true);
-    
+
     append(newline);
     QStringList::Iterator it  = lines.begin();
     QStringList::Iterator end = lines.end();
@@ -78,12 +80,12 @@ void LogPlainView::addRevision(const Cervisia::LogInfo& logInfo)
 
     setTextFormat(QStyleSheet::RichText);
 
-    for (Cervisia::LogInfo::TTagInfoSeq::const_iterator it = logInfo.m_tags.begin();
-         it != logInfo.m_tags.end(); ++it)
+    for( LogInfo::TTagInfoSeq::const_iterator it = logInfo.m_tags.begin();
+         it != logInfo.m_tags.end(); ++it )
     {
         append("<i>" + QStyleSheet::escape((*it).toString()) + "</i>");
     }
-    
+
     // add an empty line when we had tags or branches
     if( !logInfo.m_tags.empty() )
     {
@@ -91,21 +93,21 @@ void LogPlainView::addRevision(const Cervisia::LogInfo& logInfo)
         append(newline);
     }
 
-    // add horizontal line    
+    // add horizontal line
     setTextFormat(QStyleSheet::RichText);
-    append("<hr>");   
+    append("<hr>");
 }
 
 
 void LogPlainView::searchText(int options, const QString& pattern)
 {
     m_find = new KFind(pattern, options, this);
-    
+
     connect(m_find, SIGNAL(highlight(const QString&, int, int)),
             this, SLOT(searchHighlight(const QString&, int, int)));
     connect(m_find, SIGNAL(findNext()),
            this, SLOT(findNext()));
-    
+
     m_findPos = 0;
     if( options & KFindDialog::FromCursor )
     {
@@ -124,29 +126,29 @@ void LogPlainView::scrollToTop()
 
 
 void LogPlainView::findNext()
-{   
-    static const QRegExp addNewLineChar("<br[^>]*>");
-    static const QRegExp stripHtmlTags("<[^>]*>");
-    
+{
+    static const QRegExp breakLineTag("<br[^>]*>");
+    static const QRegExp htmlTags("<[^>]*>");
+
     KFind::Result res = KFind::NoMatch;
-    
+
     while( res == KFind::NoMatch && m_findPos < paragraphs() && m_findPos >= 0 )
     {
         if( m_find->needData() )
         {
             QString richText = text(m_findPos);
-            
+
             // replace <br/> with '\n'
-            richText.replace(addNewLineChar, "\n");
-            
+            richText.replace(breakLineTag, "\n");
+
             // remove html tags from text
-            richText.replace(stripHtmlTags, "");
-            
+            richText.replace(htmlTags, "");
+
             m_find->setData(richText);
         }
-   
+
         res = m_find->find();
-        
+
         if( res == KFind::NoMatch )
         {
             if( m_find->options() & KFindDialog::FindBackwards )
@@ -155,7 +157,7 @@ void LogPlainView::findNext()
                 ++m_findPos;
         }
     }
-       
+
     // reached the end?
     if( res == KFind::NoMatch )
     {
@@ -166,7 +168,7 @@ void LogPlainView::findNext()
         }
         else
         {
-            delete m_find;  
+            delete m_find;
             m_find = 0;
         }
     }
@@ -175,6 +177,7 @@ void LogPlainView::findNext()
 
 void LogPlainView::searchHighlight(const QString& text, int index, int length)
 {
+    Q_UNUSED(text);
     setSelection(m_findPos, index, m_findPos, index + length);
 }
 
@@ -184,7 +187,7 @@ void LogPlainView::setSource(const QString& name)
     if( name.isEmpty() )
         return;
 
-    const bool selectedRevisionB(name.startsWith("revB#"));
+    bool selectedRevisionB = name.startsWith("revB#");
     if( selectedRevisionB || name.startsWith("revA#") )
     {
         emit revisionClicked(name.mid(5), selectedRevisionB);
