581339564e357056bcc3068ae20d377226163de9
Christian Loose <christian.loose@hamburg.de>
2004-11-11 21:13:43 +0000

    CVS_SILENT
    add missing copyright and licence statement
    
    svn path=/trunk/kdesdk/cervisia/; revision=362316

--- resolvedlg_p.cpp
+++ resolvedlg_p.cpp
@@ -1,3 +1,15 @@
+/* 
+ *  Copyright (c) 2004 Christian Loose <christian.loose@kdemail.net>
+ *
+ * This program may be distributed under the terms of the Q Public
+ * License as defined by Trolltech AS of Norway and appearing in the
+ * file LICENSE.QPL included in the packaging of this file.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #include "resolvedlg_p.h"
 using namespace Cervisia;
 
