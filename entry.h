ce01049a6f642e01050cc02015d794d9111a512c
André Wöbbeking <woebbeking@kde.org>
2008-02-24 22:29:55 +0000

    updated copyright
    svn path=/trunk/KDE/kdesdk/cervisia/; revision=778929

--- entry.h
+++ entry.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003-2007 André Wöbbeking <Woebbeking@kde.org>
+ * Copyright (c) 2003-2008 André Wöbbeking <Woebbeking@kde.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
